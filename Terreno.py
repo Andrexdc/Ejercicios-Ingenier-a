e261a443e32417513c25ad9ef311ffa914aa83e0
andrexdc <andreschacua13@gmail.com>
2024-08-13 10:35:54 -0500

    kknion

--- Terreno.py
+++ Terreno.py
@@ -1,8 +1,13 @@
 class Terreno:
     
-    def calcularAreaTerreno(self, A, B, C):
-        areaTriangulo = (B * A) / 2
-        areaRectangulo = B * C
+    def __init__(self, A, B, C):
+        self.__A = A
+        self.__B = B
+        self.__C = C
+        
+    def calcularAreaTerreno(self):
+        areaTriangulo = (self.__B * self.__A) / 2
+        areaRectangulo = self.__B * self.__C
         areaTotal = areaTriangulo + areaRectangulo
         
         return areaTotal
